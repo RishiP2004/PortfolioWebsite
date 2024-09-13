3d2004ef415fa1acd13ec6cc13d22f7d85be8fc3
RishiPatel2004 <25939418+RishiP2004@users.noreply.github.com>
2024-09-13 19:40:01 -0400

    fix image not centered on mobile.

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -29,17 +29,14 @@ export default function HeroSection() {
 
     return (
         <div className="text-center p-10 py-10">
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 relative overflow-hidden mt-10">
                 <Image src={dp} alt="Profile picture of Rishi Patel" layout="fill" objectFit="cover" />
             </div>
-            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+            <h2 className="text-4xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
                 Rishi Patel
             </h2>
-            <h3
-                className="text-2xl py-2 dark:text-white md:text-3xl font-bold"
-                ref={typedElement}
-            />
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-blue-800">
+            <h3 className="text-xl py-2 dark:text-white md:text-3xl font-bold" ref={typedElement} />
+            <div className="text-4xl sm:text-5xl flex justify-center gap-10 py-3 text-gray-600 dark:text-blue-800">
                 <AiFillLinkedin
                     onClick={() => window.location.href = "https://linkedin.com/rishi-patel-a5622a284"}
                     className="cursor-pointer hover:text-teal-500 transition-colors"
@@ -56,4 +53,3 @@ export default function HeroSection() {
         </div>
     );
 }
-
