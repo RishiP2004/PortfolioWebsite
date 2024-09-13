3f2498ec11e976296fbe7876e06aeeff7fc83dbb
RishiPatel2004 <25939418+RishiP2004@users.noreply.github.com>
2024-09-13 18:25:39 -0400

    fix columns

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -12,8 +12,8 @@ export default function SkillsSection() {
                 <div className="text-center shadow-lg p-8 rounded-xl my-6 dark:bg-white flex-1">
                     <Image src={software} width={100} height={100} />
                     <h3 className="text-lg font-medium pt-8 pb-2">Software Skills</h3>
-                    <div className="grid grid-cols-2 gap-4">
-                        <div>
+                    <div className="flex flex-wrap justify-between">
+                        <div className="w-full sm:w-1/3 px-2">
                             <p className="py-4 font-bold">Languages</p>
                             <ul>
                                 {['Python', 'Java', 'C/C++', 'HTML', 'CSS', 'Javascript', 'SQLite', 'MySQL', 'MongoDB', 'PHP'].map((tool) => (
@@ -21,7 +21,7 @@ export default function SkillsSection() {
                                 ))}
                             </ul>
                         </div>
-                        <div>
+                        <div className="w-full sm:w-1/3 px-2">
                             <p className="py-4 font-bold">Frameworks</p>
                             <ul>
                                 {['React', 'Node', 'Next', 'Express', 'Django', 'JUnit', 'Flutter'].map((tool) => (
@@ -29,10 +29,10 @@ export default function SkillsSection() {
                                 ))}
                             </ul>
                         </div>
-                        <div className="col-span-2">
+                        <div className="w-full sm:w-1/3 px-2">
                             <p className="py-4 font-bold">Agile Methodologies</p>
                             <ul>
-                                {['Software Application Testing', 'Software Documentation Control', 'Performance Evaluation and Optimization', 'Scrum', 'Agile', 'SOLID Principles', 'Design Patterns'].map((tool) => (
+                                {['Scrum', 'Agile', 'SOLID Principles', 'Design Patterns', 'Software Testing', 'Documentation Control', 'Performance Optimization'].map((tool) => (
                                     <li className="text-gray-800 py-1" key={tool}>{tool}</li>
                                 ))}
                             </ul>
@@ -52,3 +52,5 @@ export default function SkillsSection() {
         </section>
     );
 }
+
+
